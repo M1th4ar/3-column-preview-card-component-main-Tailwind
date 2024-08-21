938197b846bda9c881e7edebe86270adca93cf00
M1th4ar <freeabdenourzoubiri@gmail.com>
2024-08-21 16:13:29 +0100

    setting-things=up

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+
+  ],
+  theme: {
+    colors: {
+      primaryBorange: 'hsl(var(--Bright-orange))', 
+      primaryCyan:'hsl(var(--Dark-cyan))',
+      primaryDcyan:'hsl(var(--Very-dark-cyan-primary))',
+      secondaryVDcyan: 'hsl(var(--Very-dark-cyan))', 
+      secondaryVLgray:'hsl(var(--Very-light-gray-background-headings-buttons))',
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
